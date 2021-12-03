935aba1ef179761195af95ee3165ac8d669763b9
qiangoo <95473223+qiangoo@users.noreply.github.com>
2021-12-03 17:38:22 +0800

    Update warp_change_ip.sh

--- warp_change_ip.sh
+++ warp_change_ip.sh
@@ -34,7 +34,7 @@ do
             sleep 3
         else
             echo -e "Region: ${region} Done, monitoring..."
-            sleep 6
+            sleep 80000
         fi
 
     elif  [[ "$result" == "000" ]];then
